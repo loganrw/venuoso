7eb9f9ff90d05495c7b2299184d5fa475caf7d9f
loganrw <loganrwatkins@hotmail.com>
2024-01-22 20:51:57 -0600

    working header. need icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    "./src/**/*.{html,scss,ts}",
+    "./src/**/*.{html,sass,ts}",
+    "./src/app/**",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'v-light-purple': '#71677c',
+        'v-purple': '#49184f',
+        'v-purple-hover': "#5a2260",
+        'v-blue': '#91a6b8',
+        'v-rose': '#945d5e',
+      },
+      fontFamily: {
+        'sans': ['"Source Sans 3"', ...defaultTheme.fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
