91fd9cd455faad457eb672a94a60044d5eef755f
loganrw <loganrwatkins@hotmail.com>
2024-02-27 21:15:14 -0600

    created intro component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { IntroSectionComponent } from './components/intro-section/intro-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    IntroSectionComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
