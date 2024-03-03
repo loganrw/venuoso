38c8562acb3279bcbb4167e8ff7531b6f5169dda
M.Venerus Desktop Windows <mikey.motorist@outlook.com>
2024-03-02 20:25:56 -0500

    Move 'showcase' component to 'components' folder.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
-import { ShowcaseComponent } from './showcase/showcase.component';
+import { ShowcaseComponent } from './components/showcase/showcase.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, ShowcaseComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    ShowcaseComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
