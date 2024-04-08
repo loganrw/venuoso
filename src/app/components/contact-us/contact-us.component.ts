133c348324f760fbd5bd61d68a1068a010e66422
H-Kelvin <163605265+H-Kelvin@users.noreply.github.com>
2024-04-08 03:57:18 -0500

    Merge with 'contactUsServiceExperiment' branch and added submit form success message

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -2,21 +2,24 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Inject } from '@angular/core';
 import { ContactFormService } from '../../services/contact-form/contact-form.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-us',
   standalone: true,
-  imports: [ReactiveFormsModule], // Import here
+  imports: [ReactiveFormsModule, CommonModule], // Import here
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.sass']
 })
 export class ContactUsComponent {
   contactForm: FormGroup;
+  submissionSuccess = false;
+  
 
   constructor(private formBuilder: FormBuilder, @Inject(ContactFormService) private contactFormService: ContactFormService) {
-    // Initialize the form structure
+
     this.contactForm = this.formBuilder.group({
-      firstName: [''], // Adding Validators is optional but recommended for basic form validation
+      firstName: [''], // Add validators
       lastName: [''],
       phoneNumber: [''],
       email: [''],
@@ -25,14 +28,23 @@ export class ContactUsComponent {
   }
 
   onSubmit(): void {
+    if(this.contactForm.valid){
+
     const firstName = this.contactForm.get('firstName')?.value;
     const lastName = this.contactForm.get('lastName')?.value;
     const phoneNumber = this.contactForm.get('phoneNumber')?.value;
     const email = this.contactForm.get('email')?.value;
     const message = this.contactForm.get('message')?.value;
-    this.contactFormService.submitForm(firstName, lastName, phoneNumber, email, message).subscribe((response: any) => {
-    }, (error: any) => {
+
+    this.contactFormService.submitForm(firstName, lastName, phoneNumber, email, message).subscribe({
+      next: (response) => {
+        this.submissionSuccess = true;
+        this.contactForm.reset();
+      },
+    error: (error) => {
       console.error(error);
+    }
     });
   }
+}
 }
