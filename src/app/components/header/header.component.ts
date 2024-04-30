df5e2b173974c68baa1fb70c3508a9ac121354a1
H-Kelvin <163605265+H-Kelvin@users.noreply.github.com>
2024-04-30 00:01:59 -0500

    Add NgZone to enable cross-module navigation from login to landing

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, HostListener, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,6 +9,8 @@ export class HeaderComponent {
   isSticky: boolean = false;
   scrollThreshold: number = 600;
 
+  constructor(private router: Router, private zone: NgZone) {}
+
   @HostListener('window:scroll', ['$event'])
   handleScroll() {
     const scrollPosition = window.scrollY || document.documentElement.scrollTop;
@@ -17,21 +19,30 @@ export class HeaderComponent {
 
   scrollToSection(event: Event, sectionId: string): void {
     event.preventDefault();
-    const section = document.getElementById(sectionId);
-    
-    section?.scrollIntoView({ behavior: 'smooth' });
-    
-    if (history.pushState) {
-      history.pushState(null, '', `#${sectionId}`);
+    if (location.pathname.includes('landing')) {
+      this.performScroll(sectionId);
     } else {
-      location.hash = sectionId;
+      this.router.navigate(['/landing']).then(() => {
+        this.zone.run(() => {
+          this.performScroll(sectionId);
+        });
+      });
     }
   }
 
-  constructor(private router: Router) {
+  performScroll(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+      if (history.pushState) {
+        history.pushState(null, '', `#${sectionId}`);
+      } else {
+        location.hash = sectionId;
+      }
+    }
   }
 
   navigateToPage(page: string) {
-    this.router.navigate(['log-in']);
+    this.router.navigate([page]);
   }
 }
