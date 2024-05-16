ccb1180e605d3c13ccdb801c9aa5525700bc380a
H-Kelvin <163605265+H-Kelvin@users.noreply.github.com>
2024-05-16 06:03:50 -0500

    Complete first mockup MVP and includes essential design elements

--- src/app/components/event-editor/event-editor.component.ts
+++ src/app/components/event-editor/event-editor.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-event-editor',
   templateUrl: './event-editor.component.html',
-  styleUrl: './event-editor.component.sass'
+  styleUrl: './event-editor.component.sass',
 })
 export class EventEditorComponent {
+  toggleCategory(event: MouseEvent) {
+    const summary = event.target as HTMLElement;
+    const ul = summary.nextElementSibling as HTMLElement;
+    const isOpen = ul.classList.contains('max-h-0');
 
+    if (isOpen) {
+      ul.classList.remove('max-h-0');
+      ul.classList.add('max-h-96'); // Adjust max height as needed
+    } else {
+      ul.classList.remove('max-h-96');
+      ul.classList.add('max-h-0');
+    }
+  }
 }
--- src/app/components/event-editor/event-editor.module.ts
+++ src/app/components/event-editor/event-editor.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { EventEditorRoutingModule } from './event-editor.routing.module';
 import { EventEditorComponent } from './event-editor.component';
+import { MatIcon } from '@angular/material/icon';
 
 @NgModule({
   declarations: [EventEditorComponent],
-  imports: [CommonModule, HttpClientModule, EventEditorRoutingModule],
+  imports: [
+    CommonModule, 
+    HttpClientModule, 
+    EventEditorRoutingModule,
+    MatIcon
+  ],
 })
 export class EventEditorModule {}
 
